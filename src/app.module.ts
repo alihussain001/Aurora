8791ee0c212d166c26f85f0209657175f6a764b3
alihussain <ahalihussain11@gmail.com>
2026-03-10 00:21:06 +0500

    chore: reorganize project into AuroraFolder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuoteModule } from './quote/quote.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('MONGO_URL') ,
+    QuoteModule],
   controllers: [AppController],
   providers: [AppService],
 })
