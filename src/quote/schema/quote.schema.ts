fdaf3f8861cf74bb9fcb8daa4c256f07d6294845
alihussain <ahalihussain11@gmail.com>
2026-03-10 20:19:42 +0500

    schema for quotes added

---
+++ src/quote/schema/quote.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type QuoteDocument = Quote & Document;
+
+@Schema({timestamps: true})
+export class Quote{
+    @Prop()
+    title!: string;
+
+    @Prop()
+    content!: string;
+    
+    @Prop()
+    author!: string;
+
+}
+
+export const QuoteSchema = SchemaFactory.createForClass(Quote);
