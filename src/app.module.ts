32489da62667773b1732cb9f7e1cc55e3519f107
alihussain <ahalihussain11@gmail.com>
2026-03-10 20:13:35 +0500

    data base setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuoteModule } from './quote/quote.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('MONGO_URL') ,
+     ConfigModule.forRoot({isGlobal: true}),
+     MongooseModule.forRootAsync({
+      inject:[ConfigService],
+        useFactory: async (configService : ConfigService) =>({
+          uri: configService.get<string>('MONGO_URI')
+        })
+     }),
     QuoteModule],
   controllers: [AppController],
   providers: [AppService],
