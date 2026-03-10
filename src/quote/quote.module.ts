62d2f3127b9d6b62382315df63a18ccb5fa83c7a
alihussain <ahalihussain11@gmail.com>
2026-03-10 20:49:59 +0500

    create quote logic added

--- src/quote/quote.module.ts
+++ src/quote/quote.module.ts
@@ -1,8 +1,13 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { QuoteController } from './quote.controller';
 import { QuoteService } from './quote.service';
+import { Quote, QuoteSchema } from './schema/quote.schema';
 
 @Module({
+  imports:[ 
+    MongooseModule.forFeature([{name: Quote.name, schema: QuoteSchema}])
+  ],
   controllers: [QuoteController],
   providers: [QuoteService]
 })
--- src/quote/quote.service.ts
+++ src/quote/quote.service.ts
@@ -1,6 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Quote } from './schema/quote.schema';
+import { Model } from 'mongoose';
+import { QuoteModule } from './quote.module';
+import { CreateQuoteDto } from './DTO/create-quotes.dto';
 
 @Injectable()
 export class QuoteService {
+    constructor(
+        @InjectModel(Quote.name)
+        private quoteModule : Model<QuoteModule>,
+    ){}
+
+    async createQuote(createQuoteDto : CreateQuoteDto){
+        return this.quoteModule.create(createQuoteDto);
+    }
 
 }
